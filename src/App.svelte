affb280147e290146da310453379d0a78c352f89
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-06 16:17:20 +0300

    Svelte Todo

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
+	// --- imports as ---
 	import { onMount } from 'svelte';
 	import Hobby from './Hobby.svelte';
 	import Button from './Button.svelte';
 	import { saveToLS, readFromLS } from './storage';
 	import { generate } from 'shortid';
 
+	// --- private variables ---
 	let id;
 	let name = '';
 	let weight = 3;
@@ -16,6 +18,94 @@
 	onMount(function() {
 		elmToFocus.focus();
 	});
+
+	// Define the look of a Hobby based on the weight/rating
+	const getHobbyLook = (weight) => {
+		let background = '#ff6b6b';
+		let color = '#FFFFFF';
+		if (weight > 3 && weight <= 6) {
+			background = '#efef04';
+			color = '#000';
+		} else if (weight > 6) {
+			background = '#0eb30e';
+			color = '#FFFFFF';
+		}
+		return {background: background, color: color};
+	}
+
+	// Method to add a hobby
+	const addHobby = () => {
+		const look = getHobbyLook(weight);
+
+		if (isEditMode) {
+			editHobby(id, name, weight, description, look);
+
+		} else {
+			const hobby = {
+				id: generate(),
+				name : name,
+				weight : weight,
+				description: description,
+				look: look
+			};
+			hobbies = hobbies.concat(hobby);
+			resetAndSave(hobbies);
+		}
+	}
+
+	// Method to delete a hobby
+	const deleteHobby = id => {
+		console.log('hobby to delete', id);
+		//find hobby by name
+		let index = hobbies.findIndex(hobby => hobby.id === id);
+		//remove hobby
+		hobbies.splice(index, 1);
+		hobbies = [...hobbies];
+		console.log('hobbies after delete', JSON.stringify(hobbies));
+		resetAndSave(hobbies);
+	};
+
+	// Method to edit a hobby
+	const editHobby = (id, newName, newWeight, newDescription, newLook) => {
+		console.log('hobby to edit', name);
+		//find hobby by name
+		let index = hobbies.findIndex(hobby => hobby.id === id);
+		//edit hobby
+		hobbies[index].name = newName;
+		hobbies[index].weight = newWeight;
+		hobbies[index].description = newDescription;
+		hobbies[index].look = newLook;
+		hobbies = [...hobbies];
+		console.log('hobbies after edit', hobbies);
+		resetAndSave(hobbies);
+	};
+
+	// Set the edit mode
+	const editMode = (hobbyId) => {
+		console.log('hobby to edit', name);
+		//find hobby by name
+		let hobby = hobbies.find(hobby => hobby.id === hobbyId);
+		id = hobby.id;
+		name = hobby.name;
+		weight = hobby.weight;
+		description = hobby.description;
+		isEditMode = true;
+	}
+
+	// Method to reset the hobby form
+	const reset = () => {
+		id = '';
+		name = '';
+		weight = 3;
+		description = '';
+		isEditMode = false;
+	}
+
+	// Method to reset and save
+	const resetAndSave = hobbies => {
+		reset();
+		saveToLS('hobbies', hobbies);
+	}
 </script>
 
 <main>
